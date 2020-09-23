84c65ea14cb48c27ff08b2d084906bb18f508bf1
Ilya Vorobyov <kotolin3000@yandex.ru>
2020-09-23 11:20:15 +0300

    Update main.py

--- LR1/main.py
+++ LR1/main.py
@@ -1,19 +1,17 @@
 import re
 
 
-reg = "(?i:mailto:[a-z0-9]+@[a-z0-9]+\.[a-z]+(\?subject=[a-z0-9]{1,64}))"
-reg_user = "(?i:: [a-z0-9]+@)"
+reg = "(?i:mailto:([a-z0-9]+)@[a-z0-9]+\.[a-z]+\?subject=[a-z0-9]{0,64})"
 in_file = open("input.txt", "r")
 out_file = open("output.txt", "w")
 user_data = {}
-data = in_file.read()
-val_list = re.findall(reg, data)
-for line in val_list:
-    user = re.findall(reg_user, line)
-    user = user[0][2:-1].lower()
-    if user[0] not in user_data:
+for line in in_file.readlines():
+    user = re.search(reg, line)
+    if user and (user not in user_data):
+        user = user.group(1)
         user_data[user] = 1
     else:
+        user = user.group(1)
         user_data[user] += 1
 for tup in user_data.items():
     out_file.write(tup[0] + ": " + str(tup[1]) + "\n")
